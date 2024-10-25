7c61ee1ab920c258dc1822e13d695f3c7a1879fd
Majo Rosas <158408316+Majorosas16@users.noreply.github.com>
2024-10-25 15:55:05 -0500

    F

---
+++ src/utils/firebaseConfig.ts
@@ -0,0 +1,11 @@
+//Solo es la llave de Firebase
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAxFkSU8-aci6dT8H1-OCYws0obwCgGD4g",
+  authDomain: "algoritmos-4a5ff.firebaseapp.com",
+  projectId: "algoritmos-4a5ff",
+  storageBucket: "algoritmos-4a5ff.appspot.com",
+  messagingSenderId: "832970424813",
+  appId: "1:832970424813:web:e198c1d5486beb620927e4",
+  measurementId: "G-XM0TRPMLBQ"
+};
